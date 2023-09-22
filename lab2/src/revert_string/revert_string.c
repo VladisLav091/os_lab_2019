276620917ce1a8e9d88f2776c436e0f89f070d1b
VladisLav091 <94039148+VladisLav091@users.noreply.github.com>
2023-09-22 14:46:07 +0000

    Поменял динамическую переменную и добавил скомпилированный код

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,8 +1,10 @@
 #include "revert_string.h"
 #include <string.h>
-
+#include <stdio.h>
 void RevertString(char *str)
 {
+    printf("Reverted1:\n");
+    printf("Reverted:\n");
 	int length = strlen(str);
     int i;
     for (i = 0; i < length / 2; i++)
